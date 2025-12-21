c19812444998f751713e66dd499f209989fb9aa5
MariaPentegova <m.pentegova@yandex.ru>
2025-12-21 17:33:35 +0300

    Create count_zero.c

---
+++ works_on_C/count_zero.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int count_zero(int *arr, int len){
+  int c = 0;
+  for (int i=0; i<len; i++){
+    if (arr[i]==0)
+      c++;
+  }
+  return c;
+}
+int main(){
+  int arr[]={2, 0, 6, 0, 0, 0, -3, 4};
+  int len = sizeof(arr)/sizeof(*arr);
+  printf("%d\n", count_zero(arr, len));
+  return 0;
+}
