1d0672d5a621a9da56397685ffe9ab5175eb0c43
MariaPentegova <m.pentegova@yandex.ru>
2025-12-26 23:08:18 +0300

    Create main.py

---
+++ python/Heap_sort/main.py
@@ -0,0 +1,23 @@
+def heapify(nums, heap_size, root_index):  
+    largest = root_index
+    l_child = (2 * root_index) + 1
+    r_child = (2 * root_index) + 2
+
+    if l_child < heap_size and nums[l_child] > nums[largest]:
+        largest = l_child
+    if r_child < heap_size and nums[r_child] > nums[largest]:
+        largest = r_child
+    if largest != root_index:
+        nums[root_index], nums[largest] = nums[largest], nums[root_index]
+        heapify(nums, heap_size, largest)
+
+def heap_sort(nums):  
+    n = len(nums)
+  
+    for i in range(n, -1, -1):
+        heapify(nums, n, i)
+    for i in range(n - 1, 0, -1):
+        nums[i], nums[0] = nums[0], nums[i]
+        heapify(nums, i, 0)
+      
+    return nums
