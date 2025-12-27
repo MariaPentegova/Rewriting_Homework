c77433810bf371fe89716129cad94db174c37adf
MariaPentegova <m.pentegova@yandex.ru>
2025-12-28 02:36:34 +0300

    Update main.c

--- src/works_on_C/more_difficult/optimal_sorting/main.c
+++ src/works_on_C/more_difficult/optimal_sorting/main.c
@@ -1,52 +1,30 @@
-#define MAX_SIZE 100
 #include <stdio.h>
 
-void swap(int *a, int *b) {
-    int t = *a;
-    *a = *b;
-    *b = t;
-}
+#define MAX_SIZE 100
 
-void quickSort(int arr[], int low, int high) {
-    if (low < high) {
-        int p = arr[high];
-        int i = low - 1;
-        for (int j = low; j < high; j++) {
-            if (arr[j] <= p) {
-                i++;
-                swap(&arr[i], &arr[j]);
-            }
-        }
-        swap(&arr[i + 1], &arr[high]);
-        int pi = i + 1;
-      
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
-    }
-}
+// Объявление функции на ассемблере
+extern int sort(int *arr, int size);
 
 int main() {
     int arr[MAX_SIZE];
     int n = 0;
+    int num;
 
-    while (n < MAX_SIZE && scanf("%d", &arr[n]) == 1) {
-        n++;
+    while (n < MAX_SIZE && scanf("%d", &num) == 1) {
+        arr[n++] ;
+        int c = getchar();
+        if (c == '\n' || c == EOF)
+            break;
+        ungetc(c, stdin); // возвращаем символ назад, если это не перенос строки
     }
 
-    int orig[MAX_SIZE];
-    for (int i = 0; i < n; i++) {
-        orig[i] = arr[i];
+    if (n == 0) {
+        return 0;
     }
 
-    quickSort(arr, 0, n - 1);
+    int changed_count = sort(arr, n);
 
-    int changed_positions = 0;
-    for (int i = 0; i < n; i++) {
-        if (orig[i] != arr[i]) {
-            changed_positions++;
-        }
-    }
+    printf("%d\n", changed_count);
 
-    printf("%d\n", changed_positions);
-    return changed_positions;
+    return changed_count;
 }
