0de66e14945564eb851b779bfd5d5345e67e6f94
MariaPentegova <m.pentegova@yandex.ru>
2025-12-28 14:11:15 +0300

    Update sorting_station.c

--- src/works_on_C/more_difficult/sorting_station.c
+++ src/works_on_C/more_difficult/sorting_station.c
@@ -4,95 +4,83 @@
 
 #define MAX_LEN 100
 
-char stack[MAX_LEN];
-int top = -1;
-
 int precedence(char op) {
     if (op == '+' || op == '-') return 1;
     if (op == '*' || op == '/') return 2;
     return 0;
 }
 
-void push(char c) {
-    if (top < MAX_LEN - 1) {
-        stack[++top] = c;
-    }
-}
-
-char pop() {
-    if (top >= 0) return stack[top--];
-    return '\0'; // если стек пуст
-}
-
-char peek() {
-    if (top >= 0) return stack[top];
-    return '\0';
-}
-
-int is_operator(char c) {
-    return (c == '+') || (c == '-') || (c == '*') || (c == '/');
-}
-
 void infix_to_postfix(const char *infix, char *postfix) {
+    // стек для операторов
+    char stack[MAX_LEN];
+    int top = -1; // стек пуст
+
     int j = 0; // индекс для postfix
+
     for (int i = 0; infix[i] != '\0'; i++) {
         char c = infix[i];
 
-        if (isspace(c)) { // пропускаем пробелы
+        if (isspace(c)) { 
             continue;
         }
 
         if (isdigit(c)) {
-            // Добавляем число в постфиксную строку
             while (isdigit(infix[i])) {
                 postfix[j++] = infix[i++];
             }
-            postfix[j++] = ' '; // разделитель
+            postfix[j++] = ' '; 
             i--; 
         } else if (c == '(') {
-            push(c);
+            if (top < MAX_LEN - 1) {
+                stack[++top] = c;
+            }
         } else if (c == ')') {
-            while (top >= 0 && peek() != '(') {
-                postfix[j++] = pop();
+            while (top >= 0 && stack[top] != '(') {
+                postfix[j++] = stack[top--];
                 postfix[j++] = ' ';
             }
-            pop();
+            if (top >= 0 && stack[top] == '(') {
+                top--;
+            }
         } else if (is_operator(c)) {
-            // извлекаем из стека все операторы с более высоким или равным приоритетом
-            while (top >= 0 && is_operator(peek()) && precedence(peek()) >= precedence(c)) {
-                postfix[j++] = pop();
+            while (top >= 0 && is_operator(stack[top]) && precedence(stack[top]) >= precedence(c)) {
+                postfix[j++] = stack[top--];
                 postfix[j++] = ' ';
             }
-            push(c);
+            if (top < MAX_LEN - 1) {
+                stack[++top] = c;
+            }
         }
     }
 
     while (top >= 0) {
-        if (peek() == '(') {
-            pop();
+        if (stack[top] == '(') {
+            top--; 
         } else {
-            postfix[j++] = pop();
+            postfix[j++] = stack[top--];
             postfix[j++] = ' ';
         }
     }
 
-    postfix[j] = '\0';
+    postfix[j] = '\0'; 
+}
+
+int is_operator(char c) {
+    return c == '+' || c == '-' || c == '*' || c == '/';
 }
 
 int main() {
     char infix[MAX_LEN];
-    char postfix[MAX_LEN*2];
+    char postfix[MAX_LEN * 2]; // резерв для разделителей и чисел
 
-    printf("Введите выражение: ");
+    printf("Enter: ");
     fgets(infix, MAX_LEN, stdin);
-  
     size_t len = strlen(infix);
-    if (len > 0 && infix[len-1] == '\n') {
-        infix[len-1] = '\0';
+    if (len > 0 && infix[len - 1] == '\n') {
+        infix[len - 1] = '\0';
     }
 
     infix_to_postfix(infix, postfix);
-
-    printf("Постфиксная форма: %s\n", postfix);
+    printf("Postfix form: %s\n", postfix);
     return 0;
 }
